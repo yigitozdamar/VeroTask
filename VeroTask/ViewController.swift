f733c92cf0bb8e4f0463ea117852c9d8079e6f53
yigit <yigitozdamar@hotmail.com>
2023-03-23 00:33:08 +0300

    Initial Commit

---
+++ VeroTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeroTask
+//
+//  Created by Yigit Ozdamar on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
